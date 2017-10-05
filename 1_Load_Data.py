9b9b4805d06a0604b2291df6a572f3bae775838b
LAPTOP-J18K6VAU\mmncit <mmncit@gmail.com>
2017-10-04 21:17:32 -0600

    Set encoding value while reading data from text file

--- 1_Load_Data.py
+++ 1_Load_Data.py
@@ -1,6 +1,7 @@
 #Load data
 import csv
-file = open('yob1910.txt','rb')
+file = open('yob1910.txt', 'rt', encoding='utf-8')
 data = csv.reader(file)
 table = [row for row in data]
 print(table[:3])
+
